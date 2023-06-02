7e67be35f0eb8756e2f018e3d76401eef571ea71
Dmitry <stetuyxa.dmitry@ya.ru>
2023-06-02 12:27:00 +0300

    bugfix

--- src/app/utils/session-maxlife.php
+++ src/app/utils/session-maxlife.php
@@ -1,8 +1,8 @@
 <?php
+require_once __DIR__ . '/../auth/logout-user.php';
 function checkSessionLife()
 {
-    if (isset($_SESSION['last_action']) && (time () - $_SESSION['last_action'] > 3600))
-    {
-        logoutUser ();
+    if (isset($_SESSION['last_action']) && (time() - $_SESSION['last_action'] > 3600)) {
+        logoutUser();
     }
 }
